3f4f467737798308a54215ec87afdfdd3d18dd58
[null] <48494379+asciid@users.noreply.github.com>
2020-07-04 00:04:52 +0300

    56 ::> 3/8

---
+++ 56/A.py
@@ -0,0 +1,4 @@
+a, b, c = list(map(int, input().split()))
+print("{}+{}+{}={}".format(a, b, c, a+b+c))
+print("{}*{}*{}={}".format(a, b, c, a*b*c))
+print("({}+{}+{})/3={:.3f}".format(a, b, c, (a+b+c)/3))
