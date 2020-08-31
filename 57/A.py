2de66e6e9014f88cabbb3aad240637b072afb808
[null] <48494379+asciid@users.noreply.github.com>
2020-08-31 16:57:30 +0300

    Update A.py

--- 57/A.py
+++ 57/A.py
@@ -1,26 +1,3 @@
 a, b, c = list(map(int, input().split()))
-
-if a > b:
-	if b > c:
-		high = a
-		low  = c
-	else:
-		high = a
-		low  = b
-elif b > c:
-	if c > a:
-		high = b
-		low  = a
-	else:
-		high = b
-		low  = c
-elif c > a:
-	if a > b:
-		high = c
-		low  = b
-	else:
-		high = c
-		low  = a
-
-print(low)
-print(high)
+print(min(a, b, c))
+print(max(a, b, c))
